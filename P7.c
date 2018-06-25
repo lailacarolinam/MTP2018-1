984483cd2273c9890d6609572875792852a205df
lailacarolinam <37491070+lailacarolinam@users.noreply.github.com>
2018-06-25 15:18:02 -0300

    Update P7.c

--- P7.c
+++ P7.c
@@ -2,25 +2,20 @@
 //11721EEL022
 
 #include <stdio.h>
-
-int Ack(m,n){
-    switch(m){
-        case 0: 
+int A(int m, int n)
+{
+    if (m==0)
         return n+1;
-        default:
-            if(n==0)
-               return Ack(m-1,1);
-            else
-               return Ack(m-1,Ack(m,n-1));
-    }
+    else if (m>0 && n==0)
+        return A(m-1, 1);
+    else if (m>0 && n>0)
+        return A(m-1, A(m, n-1));
 }
-
-int main()
-{   
-    int m, n;
-    printf("Digite os dois numeros inteiros da funcao de Ackermann: ");
-    scanf("%d", &m); getchar();
-    scanf("%d", &n); getchar();
-    printf("\n\nO resultado de %d e %d foi: %d",m, n, Ack(m,n));
-    return 0;
+int main ()
+{
+	int n=0, m=0;
+	printf("Valores para n e m:\n");
+	scanf("%d %d", &m, &n);
+	printf("%d", A(m, n));
+	return 0;
 }
