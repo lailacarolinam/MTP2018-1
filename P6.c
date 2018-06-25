d3c50c2c9855cada6d945700af2308fa24256b96
lailacarolinam <37491070+lailacarolinam@users.noreply.github.com>
2018-06-25 15:17:17 -0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -2,35 +2,26 @@
 //11721EEl022
 
 #include <stdio.h>
-
-int soma(int num[], int quant){
-    if(quant==0)
-        return num[quant];
-    else 
-        return (num[quant]+soma(num,quant-1));
-}
-
-float Media(Sum,quant){
-    float M;
-    M = Sum/quant;
-    return M;
-}
-
-int main()
+int sm(int len, int *v)
 {
-    int num[20], i, N, s=0;
-    do {
-        printf("\nDigite quantos numeros devem ter no vetor, entre 5 e 10: ");
-        scanf("%d", &N);
-        getchar();    
-    }while(N<5 || N>20);
-    printf("\n\nDigite os do vetor: ");
-    for(i=0; i<=20; i++)
-        num[i]=0;
-    for(i=0; i<N; i++){
-        scanf("%d", &num[i]);
-        getchar();
-    }
-    printf("A media e: %.4f", Media(soma(num,N),N));
-    return 0;    
+    if(len>0)
+        return *v+sm(len-1, v+1);
+    else
+        return 0.0;
+}
+float med(int x, int y)
+{
+    return (float)x/(float)y;
+}
+int main ()
+{
+	int vet[]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+	int tam=0, i=0;
+	printf("Quantidade a ser somada:\n");
+	scanf("%d", &tam);
+	printf("\nValores: \n");
+	for (i=0; i<tam; i++) 
+		scanf("%d", &vet[i]);
+	printf("\nmedia = %f", med(sm(tam, vet), tam));
+	return 0;
 }
