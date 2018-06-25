10ea1d75cf42710ead48892112cb0a13d84196b9
lailacarolinam <37491070+lailacarolinam@users.noreply.github.com>
2018-06-25 15:15:10 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,27 +1,18 @@
 //Laila Carolina de Paula Miranda 11721EEL022
 
 #include <stdio.h>
-
 int main()
 {
-    char a[257];
-    int num , i=0 ,cum=0;
-    printf("Digite um numero inteiro a seguir: ");
-    scanf("%s", &a);
-    getchar();
-    while(a[i]!='\0'){
-        if(a[i]>='0' && a[i]<='9'){
-            num=a[i]-'0';
-            cum+=num;
-            cum*=10;
-            i++;
-        }
-        else{
-            cum+=0;
-            i++;
-        }
-    }
-    cum/=10;
-    printf("\nSeu valor e: %d.",cum);
-    return 0;
+	int conv=0, cum=0, i=0;
+	char numero[256];
+	scanf("%s", numero);
+	getchar();
+	for (i=0; numero[i]; i++)
+	{
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+			cum=cum*10+(numero[i]-'0');
+		else;
+	}
+	printf("%i", cum);
+	return 0;
 }
